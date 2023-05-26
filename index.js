e82d10783b75f185b2f5846078ca272e5a778838
Eduardo Díaz <eduaedodiaz@gmail.com>
2023-05-25 18:36:00 -0600

    cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
+// const cors = require('cors');
 const routerApi = require('./routes');
+// const boom = require('@hapi/boom');
 
 const {
   logErrors,
@@ -12,6 +14,18 @@ const port = 3000;
 
 app.use(express.json());
 
+// const whitelist = ['http://localhost:3000'];
+// const options = {
+//   origin: (origin, callback) => {
+//     if (whitelist.includes(origin)) {
+//       callback(null, true);
+//     } else {
+//       callback(boom.unauthorized());
+//     }
+//   },
+// };
+// app.use(cors(options));
+
 app.get('/', (req, res) => {
   res.send('Hola, mi server en express');
 });
@@ -20,9 +34,7 @@ app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.listen(port, () => {
-  console.log(`Mi puerto Localhost:${port}`);
-});
+app.listen(port, () => {});
 routerApi(app);
 // Utilizamos los middleware. Siempre deben ir después del routing:
 app.use(logErrors);
